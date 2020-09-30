48e51dad53bc99767b6f431a6c366a63f49642be
Ivan <citrunin73@gmail.com>
2020-09-30 15:08:39 +0400

    Initial commit

---
+++ src/Lesson1/Main1.java
@@ -0,0 +1,72 @@
+package Lesson1;
+
+public class Main1 {
+    public static void main(String[] args) {
+//    1)
+        byte a = Byte.MAX_VALUE;
+        short b = Short.MAX_VALUE;
+        int c = Integer.MAX_VALUE;
+        long d = Long.MAX_VALUE;
+        float e = Float.MAX_VALUE;
+        double f = Double.MAX_VALUE;
+        char j = Character.MAX_VALUE;
+        boolean h = false;
+//    2)
+        double i = Result(a, b, c, d);
+//    3)
+        boolean k = Result1(a, b);
+//    4)
+        int s = 5;
+        printResult(s);
+//    5)
+        boolean y = Result3(a);
+        String n = "Иван";
+        printName(n);
+//    6)
+        int year = 2020;
+        printHear(year);
+
+
+
+    }
+
+    static double Result(double a, double b, double c, double d) {
+        return a * (b + (c / d));
+    }
+
+    static boolean Result1(double a, double b){
+        if ((a+b)>=10 && (a+b)<=20){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    static void printResult(int s){
+        if (s >= 0) {
+            System.out.println("Это положительное число");
+        } else {
+            System.out.println("Это отрицательное число");
+        }
+    }
+
+    static boolean Result3(double a){
+        if (a < 0){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    static void printName(String n){
+        System.out.println("Привет, " + n + "!");
+    }
+
+    static void printHear(int year){
+        if ((year % 400 == 0) || (year % 4 == 0 && year % 100!=0)){
+            System.out.println("Этот год високосный");
+        } else {
+            System.out.println("Этот год не високосный");
+        }
+    }
+}
---
+++ src/Lesson2/Main.java
@@ -0,0 +1,85 @@
+package Lesson2;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int arr [] = {1,1,0,0,1,0,1,1,0,0};
+        printArr(arr);
+        int arr1 [] = new int[8];
+        printArr1(arr1);
+        int arr2 [] [] = new int [5] [5];
+        printArr2(arr2);
+        int arr3 [] = {10,55,-15,13,16};
+        int max=0;
+        printArr3max(arr3,max);
+        int min=arr3[0];
+        printArr3min(arr3,min);
+        boolean c = printCheckBalance(arr3);
+    }
+
+    public static void printArr(int arr []){
+        for (int i = 0; i < arr.length; i++) {
+            if( arr[i]==1){
+                arr[i]=0;
+            } else {
+                arr[i]=1;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void printArr1(int arr1 []){
+        for (int i = 1; i < arr1.length; i++) {
+            arr1[i]  = (arr1[i-1]+3);
+        }
+        System.out.println(Arrays.toString(arr1));
+    }
+
+    public static void printArr2(int arr2 [] []){
+        for (int i = 0;i < arr2.length ; i++) {
+            for (int j = 0;j < arr2[i].length ; j++){
+                if (i==j){
+                    arr2[i][j]=1;
+                } else if (i+j== arr2.length-1){
+                    arr2[i][j]=1;
+                }
+                System.out.print(arr2[i][j]+"\t");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void printArr3max(int arr3 [], int max) {
+        for (int i = 0;i < arr3.length; i++){
+            if (max < arr3[i]){
+                max = arr3[i];
+            }
+        } System.out.println("ArrayMax= " + max);
+
+    }
+
+    public static void printArr3min(int arr3 [], int min) {
+        for (int i = 0;i < arr3.length; i++){
+            if (min > arr3[i]){
+                min = arr3[i];
+            }
+        } System.out.println("ArrayMin= " + min);
+
+    }
+
+    public static boolean printCheckBalance(int[] arr3) {
+        int a = 0;
+        int b = 0;
+        for (int i = 0; i < arr3.length ; i++) {
+            a = a + arr3[i];
+            b = b + arr3[arr3.length-(i+1)];
+            if (a == b) {
+                System.out.println(true);
+                return true;
+            }
+        }
+        System.out.println(false);
+        return false;
+    }
+}
